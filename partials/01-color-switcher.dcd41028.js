f7949e652f2c0681364a1c46426daea1a2c60f63
Zhe1a <108029808+Zhe1a@users.noreply.github.com>
2022-09-28 22:31:16 +0000

    Deploying to gh-pages from @ Zhe1a/goit-js-hw-09@bd77553ec0d550e7cfd5fd7d0a8e8e053227adea 🚀

---
+++ partials/01-color-switcher.dcd41028.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),d=document.querySelector("body"),n=null;function a(){d.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}t.disabled=!1,e.disabled=!0,t.addEventListener("click",(function(){n=setInterval(a,1e3),t.disabled=!0,e.disabled=!1})),e.addEventListener("click",(function(){t.disabled=!1,e.disabled=!0,clearInterval(n)}))}();
+//# sourceMappingURL=01-color-switcher.dcd41028.js.map
