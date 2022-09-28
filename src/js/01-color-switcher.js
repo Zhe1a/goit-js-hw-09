dbf41438f5295d6f288ebec4a1a9793d41a97ee9
Zhe1a <mgr58006@gmail.com>
2022-09-29 01:05:07 +0300

    fixs

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,24 +1,45 @@
-const start = document.querySelector("[data-start]");
-const end = document.querySelector('[data-stop]');
+const start = document.querySelector('button[data-start]');
+const endStop = document.querySelector('button[data-stop]');
 const body = document.querySelector('body');
-function button() {
-// console.log(params.target.textContent);
-// console.log(params.target.dataset.start);
-    timer()  
- console.log("start");
-}
-function endColorInterval() {
- timer()
+console.log(endStop);
+start.disabled = false
+endStop.disabled = true
+let timeId = null
 
-}
-
-function timer(e) {
-    setInterval(()=>{
-        body.style.backgroundColor = getRandomHexColor()
-    }, 1000)
+function timer() {
+    body.style.backgroundColor = getRandomHexColor()
 }
 function getRandomHexColor() {
-    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-  }
-  end.addEventListener('click',endColorInterval);
-  start.addEventListener('click',button);
+   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+ }
+
+
+start.addEventListener('click',button);
+
+function button() {
+    timeId = setInterval(timer,1000)
+start.disabled = true
+endStop.disabled = false
+}
+
+
+
+endStop.addEventListener('click',endColorInterval);
+
+function endColorInterval() {
+    console.log("folss");
+    start.disabled = false
+    end.disabled = true
+    setInterval(timeId)
+}
+
+
+
+
+
+
+
+
+
+  // console.log(params.target.textContent);
+// console.log(params.target.dataset.start);
